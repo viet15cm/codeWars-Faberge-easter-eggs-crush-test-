0090f1ccc727f121f6c2f92500f0eec41ce3d6c6
Administrator <Administrator@SHARE10S-PC>
2019-10-02 12:37:34 +0700

    Add project files.

---
+++ Thả Trứng/Faberge.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Thả_Trứng
+{
+    class Faberge
+    {
+        public static BigInteger mo = new BigInteger(998244353);
+        public static BigInteger Height(BigInteger n, BigInteger m)
+        {
+            /*
+             * Create init list
+             */
+            List<BigInteger> PromisesNoList = new List<BigInteger>();
+            PromisesNoList.Add(0);
+            PromisesNoList.Add(1);
+            for (int i = 2; i < 80001; i++)
+            {
+                PromisesNoList.Add((BigInteger)(mo - mo / i) * (BigInteger)(PromisesNoList[(int)mo % i]) % mo);
+               
+            }
+
+            /*
+             * Logic
+             */
+            BigInteger h = 0;
+            BigInteger t = 1;
+            m = m % mo;
+            for (int i = 1; i < n + 1; i++)
+            {
+                t = (BigInteger)t * (m - i + 1) * PromisesNoList[i] % mo;
+                h = (BigInteger)(h + t) % mo;
+            }
+            return h % mo;
+        }
+    }
+
+}
---
+++ Thả Trứng/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using Thả_Trứng;
+/*
+* Create by: Promises No
+* Date: 06/09/2019
+*/
+public class Program
+{
+    
+    public static void Main()
+    {
+
+
+        Faberge.Height((BigInteger)1, (BigInteger)1);
+        Console.WriteLine("ket qua : {0}", Faberge.Height((BigInteger)2, (BigInteger)14) );
+        
+
+        Console.ReadKey();
+    }
+    
+}
